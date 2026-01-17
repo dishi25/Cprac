99f64c28d34ce0a1212f809095aa35e5dc40b659
Dishika <dishikatab@gmail.com>
2026-01-17 22:59:52 +0530

    added what I learnt day1.cpp
    Implemented basic input and output in C++.

---
+++ day1.cpp
@@ -0,0 +1,9 @@
+#include<iot.stream>
+using namespace std;
+int main(){
+  int x;
+cin >>x;
+cout << "Value of x is:" << x;
+return 0;
+}
+##i learnt how to take basic input and output in cpp. i learnt using namespace std makes it such that we dont have to use std :: again.
