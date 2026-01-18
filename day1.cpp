19337d5e8446bf77b8cbb174c54455025e4c74ef
Dishika <dishikatab@gmail.com>
2026-01-18 13:44:08 +0530

    day1.cpp
    Updated the program to take multiple inputs and display their values.

--- day1.cpp
+++ day1.cpp
@@ -6,4 +6,13 @@ cin >>x;
 cout << "Value of x is:" << x;
 return 0;
 }
-##i learnt how to take basic input and output in cpp. i learnt using namespace std makes it such that we dont have to use std :: again.
+//i learnt how to take basic input and output in cpp. i learnt using namespace std makes it such that we dont have to use std :: again.
+//let us take multiple inputs.
+#include<iot.stream>
+using namespace std;
+int main(){
+  int x, y;
+cin >>x >> y;
+cout << "Value of x is:" << x << "value of y is:" << y <<;
+return 0;
+}  
