feaaf06dfe861b06deae52b7759c17aa585eaa0c
Dishika <dishikatab@gmail.com>
2026-01-19 22:29:51 +0530

    day 1 of week1.c
    Fix syntax errors and improve code structure.

--- week1.c
+++ week1.c
@@ -1,1 +1,61 @@
+//DAY 1 OF C FROM BASICS TO HIGH-LEVEL.
+
+#include <stdio.h>
+int main(void)
+{
+  printf("Hello world")
+  return 0;
+}
+
+#include <stdio.h>
+int main(){
+int var:
+var =10;
+printf("%d", var);
+return 0:
+  }
+
+// write a function
+#include <stdio.h>
+int sum(int a, int b){
+return a+b;
+}
+int main(){
+  printf("%d", sum(10,20));
+  return 0;
+}
+
+#include <stdio.h>
+int main(){
+  int a = 25;
+ float b = 5.7;
+ char grade = 'S';
+printf("age of the person is %d\n",a);
+printf("random decimal number is %.1f\n",b);
+// here we wrote %.1f instead of %f to indicate that there is only one digit after the decimal point
+// default %f has a precision of 4 digits
+printf("the grades that i will get in all subjects anll semesters is %c\n",grade)
+  return 0;
+}
+
+//we can use initialization also.
+int main(){
+  int a = 25;
+ float b = 5.7;
+ char grade;
+ grade = 'S';
+printf("age of the person is %d\n",a);
+printf("random decimal number is %.1f\n",b);
+printf("the grades that i will get in all subjects anll semesters is %c\n",grade);
+return 0;
+}
+
+//acessing variables in C
+#include <stdio.h>
+int main(){
+  int num =25;
+ printf("we are accessing the number stored in the variable that is %d",num);
+return 0;
+}
+  
 
