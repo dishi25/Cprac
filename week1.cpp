5cafed624764fc9699385c253fa3efe2489a7f22
Dishika <dishikatab@gmail.com>
2026-01-20 21:31:25 +0530

    Fix include statements and improve input/output examples

--- week1.cpp
+++ week1.cpp
@@ -1,4 +1,4 @@
-#include<iot.stream>
+#include<iostream>
 using namespace std;
 int main(){
   int x;
@@ -9,7 +9,7 @@ return 0;
 //i learnt how to take basic input and output in cpp. i learnt using namespace std makes it such that we dont have to use std :: again.
 
 //let us take multiple inputs.
-#include<iot.stream>
+#include<iostream>
 using namespace std;
 int main(){
   int x, y;
@@ -18,7 +18,8 @@ cout << "Value of x is:" << x << "value of y is:" << y <<;
 return 0;
 }  
 
-#include <iot.stream>
+//DAY 2
+#include <iostream>
 using namespace std:
 int main(){
   cout << "Hello world";
@@ -33,10 +34,24 @@ cout<< var;
 return 0;
 }
 
-#include <iot.stream>
+#include <iostream>
 using namespace std;
 int main(){
   char c = 'S';
 cout<< c;
   return 0;
 }
+
+#include <iostream>
+using namespace std;
+int main(){
+  int num = 10;
+  cout << num <<end1; ??end1 after the cout command just gives the output a new line.
+num = 0x15;
+cout<< num;
+  return 0;
+}
+
+
+
+
