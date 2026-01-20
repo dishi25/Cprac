af4ae5c52ce7e6ce6e5e1a18e46d5f7ec2f5a2e9
Dishika <dishikatab@gmail.com>
2026-01-20 21:20:34 +0530

    Fix input/output and add Hello World example

--- week1.cpp
+++ week1.cpp
@@ -7,6 +7,7 @@ cout << "Value of x is:" << x;
 return 0;
 }
 //i learnt how to take basic input and output in cpp. i learnt using namespace std makes it such that we dont have to use std :: again.
+
 //let us take multiple inputs.
 #include<iot.stream>
 using namespace std;
@@ -16,3 +17,11 @@ cin >>x >> y;
 cout << "Value of x is:" << x << "value of y is:" << y <<;
 return 0;
 }  
+
+#include <iot.stream>
+using namespace std:
+int main(){
+  cout << "Hello world";
+return 0;
+}
+
